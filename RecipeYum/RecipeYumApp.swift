e01ff115cd708dd3b8c17bd0ae23a388061d5a0c
atsuki <csatsfapsfu2930@gmail.com>
2023-10-02 11:36:26 -0700

    Initial Commit

---
+++ RecipeYum/RecipeYumApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeYumApp.swift
+//  RecipeYum
+//
+//  Created by Atsuki on 2023-10-02.
+//
+
+import SwiftUI
+
+@main
+struct RecipeYumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
