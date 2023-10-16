031c3abefab312755b52f60a30358cf99cd40610
atsuki <csatsfapsfu2930@gmail.com>
2023-10-16 16:02:41 -0700

    added extension

--- RecipeYum/Extensions/DictionaryUrl.swift
+++ RecipeYum/Extensions/DictionaryUrl.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+extension Dictionary where Key == String, Value == String {
+    var urlEncodedString: String {
+        let pairs = self.map { key, value in
+            return "\(key)=\(value.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)"
+        }
+        return pairs.joined(separator: "&")
+    }
+}
