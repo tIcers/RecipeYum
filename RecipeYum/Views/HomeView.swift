f62eb490dd237366e3fdf57f427df558905dc088
atsuki <csatsfapsfu2930@gmail.com>
2023-11-13 18:29:21 -0800

    updated homeView

--- RecipeYum/Views/HomeView.swift
+++ RecipeYum/Views/HomeView.swift
@@ -6,34 +6,47 @@
 //
 
 import SwiftUI
+
 struct HomeView: View {
     @ObservedObject var viewModel = ImageListViewModel()
     @State private var searchText = ""
+
     var body: some View {
         NavigationView {
             let columns = [
                 GridItem(.flexible()),
                 GridItem(.flexible())
             ]
-            ScrollView{
+
+            ScrollView {
                 LazyVGrid(columns: columns) {
                     ForEach(viewModel.images) { image in
-                        NavigationLink(destination: ImageDetailPage(imageModel: image)) { // Update this line
+                        NavigationLink(destination: ImageDetailPage(imageModel: image)) {
                             ImageView(imageURL: image.imageURL)
                                 .cornerRadius(10)
                                 .padding(8)
                         }
                     }
                 }
-                .navigationTitle("Today's Meals")
-                .onAppear {
+            }
+            .navigationTitle("Today's Meals")
+            .searchable(text: $searchText, prompt: "Search food...")
+            .onChange(of: searchText) { newValue in
+                if newValue.isEmpty {
+                    viewModel.fetchRandomImages()
+                } else {
+                    viewModel.fetchRecipes(for: newValue)
+                }
+            }
+            .onAppear {
+                if searchText.isEmpty {
                     viewModel.fetchRandomImages()
                 }
             }
-            .searchable(text: $searchText, prompt: "Search food...")
         }
     }
 }
+
 
 #Preview {
     HomeView()
