10b84308c75ec78865390c0d3dc6ef852c6e6d97
atsuki <csatsfapsfu2930@gmail.com>
2023-11-13 20:25:44 -0800

    refactored code

--- RecipeYum/Views/HomeView.swift
+++ RecipeYum/Views/HomeView.swift
@@ -10,24 +10,29 @@ import SwiftUI
 struct HomeView: View {
     @ObservedObject var viewModel = ImageListViewModel()
     @State private var searchText = ""
+    @State private var favorites: Set<UUID> = []
+
+    private var gridView: some View {
+        let columns = [
+            GridItem(.flexible()),
+            GridItem(.flexible())
+        ]
+
+        return LazyVGrid(columns: columns) {
+            ForEach(viewModel.images) { image in
+                NavigationLink(destination: ImageDetailPage(imageModel: image)) {
+                    ImageView(imageURL: image.imageURL, title: image.label, isFavorite: favoriteBinding(for: image.id))
+                        .cornerRadius(10)
+                        .padding(8)
+                }
+            }
+        }
+    }
 
     var body: some View {
         NavigationView {
-            let columns = [
-                GridItem(.flexible()),
-                GridItem(.flexible())
-            ]
-
             ScrollView {
-                LazyVGrid(columns: columns) {
-                    ForEach(viewModel.images) { image in
-                        NavigationLink(destination: ImageDetailPage(imageModel: image)) {
-                            ImageView(imageURL: image.imageURL)
-                                .cornerRadius(10)
-                                .padding(8)
-                        }
-                    }
-                }
+                gridView
             }
             .navigationTitle("Today's Meals")
             .searchable(text: $searchText, prompt: "Search food...")
@@ -45,7 +50,22 @@ struct HomeView: View {
             }
         }
     }
+
+    private func favoriteBinding(for id: UUID) -> Binding<Bool> {
+        Binding<Bool>(
+            get: { self.favorites.contains(id) },
+            set: { isFavorite in
+                if isFavorite {
+                    self.favorites.insert(id)
+                } else {
+                    self.favorites.remove(id)
+                }
+            }
+        )
+    }
 }
+
+
 
 
 #Preview {
