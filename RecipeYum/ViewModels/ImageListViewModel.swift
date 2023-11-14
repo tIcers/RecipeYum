32d6504e4c7ead1f6e74c49a36e0c3918b9d8e14
atsuki <csatsfapsfu2930@gmail.com>
2023-11-13 18:29:06 -0800

    modifed viewmodels for searhcing queries

--- RecipeYum/ViewModels/ImageListViewModel.swift
+++ RecipeYum/ViewModels/ImageListViewModel.swift
@@ -10,21 +10,24 @@ import Combine
 
 class ImageListViewModel: ObservableObject {
     @Published var images: [ImageModel] = []
+    @Published var searchString: String = ""
 
-    func fetchRandomImages() {
-        let appId = "f2f45e95"
-        let appKey = "b72dc6cc95bcc5c2731e9880f4436877"
-        let apiEndpoint = "https://api.edamam.com/api/recipes/v2"
+    private var cancellables: Set<AnyCancellable> = []
+    private let appId = "f2f45e95"
+    private let appKey = "b72dc6cc95bcc5c2731e9880f4436877"
+    private let apiEndpoint = "https://api.edamam.com/api/recipes/v2"
 
+    func fetchRecipes(for query: String) {
         let parameters: [String: String] = [
-            "q": "chicken",
+            "q": query,
             "app_id": appId,
             "app_key": appKey,
-            "type": "public",
-            "random": "true"
+            "type": "public"
         ]
 
-        URLSession.shared.dataTaskPublisher(for: URL(string: apiEndpoint + "?" + parameters.urlEncodedString)!)
+        guard let url = URL(string: apiEndpoint + "?" + parameters.urlEncodedString) else { return }
+
+        URLSession.shared.dataTaskPublisher(for: url)
             .map(\.data)
             .decode(type: EdamamAPIResponse.self, decoder: JSONDecoder())
             .receive(on: DispatchQueue.main)
@@ -33,10 +36,10 @@ class ImageListViewModel: ObservableObject {
                 case .finished:
                     break
                 case .failure(let error):
-                    print("Error fetching random images: \(error)")
+                    print("Error fetching recipes: \(error)")
                 }
             }, receiveValue: { [weak self] response in
-                let randomImages = response.hits.map { hit in
+                self?.images = response.hits.map { hit in
                     ImageModel(
                         imageURL: URL(string: hit.recipe.image)!,
                         label: hit.recipe.label,
@@ -51,11 +54,13 @@ class ImageListViewModel: ObservableObject {
                         protein: hit.recipe.totalNutrients.PROCNT.quantity
                     )
                 }
-                self?.images = randomImages.shuffled()
             })
             .store(in: &cancellables)
     }
+    func fetchRandomImages() {
+        let randomQueries = ["chicken", "pasta", "salad", "dessert"]
+        let randomQuery = randomQueries.randomElement() ?? "chicken"
 
-    private var cancellables: Set<AnyCancellable> = []
-}
+        fetchRecipes(for: randomQuery)
+    }}
 
